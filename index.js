faa24e53f35169c76f02500cc5ab0ed295a8611a
nelson <114460782+sito-mi-37@users.noreply.github.com>
2022-10-21 00:10:23 +0000

    changed function parameter from range to upperRange

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 let username = prompt("enter username!")
 
 
-function guess(range, score = 1) {
-  const secretNum = Math.ceil(Math.random() * range)
+function guess(upperRange, score = 1) {
+  const secretNum = Math.ceil(Math.random() * upperRange)
   let newTurn = 0;
   let hasWon = false;
   console.log(`STAGE: ${score}`)
 
-  while (userNum != secretNum && newTurn < 2 && range < 5) {
+  while (userNum != secretNum && newTurn < 2 && upperRange < 5) {
     newTurn++;
-    var userNum = parseInt(prompt(`guess a number from 1 - ${range} `))
+    var userNum = parseInt(prompt(`guess a number from 1 - ${upperRange} `))
 
     if (userNum == secretNum) {
       console.log('you got it right');
@@ -25,14 +25,14 @@ function guess(range, score = 1) {
 
   if (hasWon == true) {
     console.log(`Congratulations ${username} | Total Score: ${score}`)
-    if (range < 5) {
-      guess(range += 1, score += 1)
+    if (upperRange < 5) {
+      guess(upperRange += 1, score += 1)
     }
-  } else if (range === 5) {
+  } else if (upperRange === 5) {
     console.log("hurray! you have finished all levels")
   } else if (hasWon == false) {
     console.log(`sorry ${username}, better luck next time!`)
     return;
   }
 }
-guess(2, 1)
+guess(2)
